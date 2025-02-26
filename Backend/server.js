54eec5579609d1fc4c2ebfab600e40f7ba84b8df
Akash S <akashspalloor@gmail.com>
2025-02-26 12:07:35 +0530

    updated url

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "https://blogspotmern.vercel.app/",
+    origin: "https://blogspotmern.vercel.app",
     credentials: true,
   }));
 

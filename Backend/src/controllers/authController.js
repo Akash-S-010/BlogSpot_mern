709485d052037c4f99966ae304db6fa198605209
Akash S <akashspalloor@gmail.com>
2025-02-26 12:21:45 +0530

    token updated

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -98,8 +98,8 @@ export const login = async (req, res) => {
         // Set access token in cookie
         res.cookie("accessToken", accessToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "lax",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days in milliseconds
         });
 
